06fd591746d3fa4ebd0542af3a334c2361c1e878
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2023-04-13 17:45:34 -0500

    Improve examples

--- examples/helloworld/src/lib.rs
+++ examples/helloworld/src/lib.rs
@@ -1,57 +1,44 @@
-use std::mem;
+use std::{cell::RefCell, rc::Rc};
 
 use js_sys::Math;
 use wasm_bindgen::prelude::*;
-use babylon::prelude::*;
+use babylon::{prelude::*, api};
 use web_sys::console;
 
-/* 
-#[macro_use]
-extern crate lazy_static;
-use std::sync::Mutex;
-lazy_static! {
-    static ref GAME: Mutex<Game> = Mutex::new(Game::new());
-}
-
 struct Game {
-    scene: Scene,
-    shape: Vec<Sphere>,
+    scene: Rc<RefCell<Scene>>,
+    shapes: Vec<BabylonMesh>,
 }
 
 impl Game {
     fn new() -> Self {
         Game {
-            scene: Scene::create_from_basic_engine("#renderCanvas"),
-            shape: vec![],
+            scene: api::create_basic_scene("#renderCanvas"),
+            shapes: vec![],
         }
     }
-}*/
+}
+
+thread_local! {
+    static GAME: RefCell<Game> = RefCell::new(Game::new());
+}
 
 #[wasm_bindgen(start)]
 pub fn main() {
-    let scene = babylon::api::create_basic_scene("#renderCanvas");
-    
-    console::log_1(&"Started".into());
+    console::log_1(&"Starting demo...".into());
 
-    /*babylon::js::log("Starting demo...");
-    let mut game = GAME.lock().unwrap();
-    for _ in 0..10 {
-        let mut sphere = Sphere::new(&game.scene, babylon::js::random());
-        sphere.set_position(Vector::new(
-            babylon::js::random() - 0.5,
-            babylon::js::random() - 0.5,
-            babylon::js::random() - 0.5,
-        ));
-        game.shape.push(sphere);
-    }*/
+    GAME.with(|game| {    
 
-    for i in 0..10 { 
-        let sphere = BabylonMesh::create_sphere(&scene.borrow(), format!("sphere_{}", i).as_str(), SphereOptions{ diameter: Some(Math::random() + 0.5), ..Default::default() });
-        sphere.set_position(Vector3::new(
-            Math::random() - 0.5,
-            Math::random() - 0.5,
-            Math::random() - 0.5,
-        ));        
-        mem::forget(sphere);
-    }
+        for i in 0..10 { 
+            let sphere = BabylonMesh::create_sphere(&game.borrow().scene.borrow(), format!("sphere_{}", i).as_str(), SphereOptions{ diameter: Some(Math::random() + 0.5), ..Default::default() });
+            sphere.set_position(Vector3::new(
+                Math::random() - 0.5,
+                Math::random() - 0.5,
+                Math::random() - 0.5,
+            ));        
+            GAME.with(|game| {
+                game.borrow_mut().shapes.push(sphere);
+            });
+        }
+    });
 }
--- examples/materials/src/lib.rs
+++ examples/materials/src/lib.rs
@@ -1,61 +1,56 @@
 use babylon::{prelude::*, api};
 use js_sys::Math;
-use wasm_bindgen::{JsValue, prelude::wasm_bindgen};
+use wasm_bindgen::prelude::wasm_bindgen;
 use web_sys::console;
-#[macro_use]
-extern crate lazy_static;
-use std::{sync::{Mutex, Arc}, cell::RefCell, rc::Rc};
+use std::{cell::RefCell, rc::Rc};
 
-// lazy_static! {
-//     static ref GAME: Mutex<Game> = Mutex::new(Game::new());
-// }
+struct Game {
+    scene: Rc<RefCell<Scene>>,
+    shapes: Vec<BabylonMesh>,
+}
 
-// struct Game {
-//     scene: Arc<Mutex<Scene>>,
-//     shape: Vec<BabylonMesh>,
-// }
-
-// impl Game {
-//     fn new() -> Self {
-//         Game {
-//             scene: api::create_basic_scene("#renderCanvas"),
-//             shape: vec![],
-//         }
-//     }
-// }
+impl Game {
+    fn new() -> Self {
+        Game {
+            scene: api::create_basic_scene("#renderCanvas"),
+            shapes: vec![],
+        }
+    }
+}
 
 thread_local! {
-    static SHAPES: RefCell<Vec<BabylonMesh>> = RefCell::new(Vec::new());
+    static GAME: RefCell<Game> = RefCell::new(Game::new());
 }
+
 
 #[wasm_bindgen(start)]
 pub fn main() {
     console::log_1(&"Starting demo...".into());
-    //let mut game = GAME.lock().unwrap();
-    let scene = api::create_basic_scene("#renderCanvas");
-    for i in 0..10 {
-        let mut cube = BabylonMesh::create_box(
-            &scene.borrow(),
-            format!("box_{}", i).as_str(),
-            BoxOptions { depth: Math::random().into(), height: Math::random().into(), width: Math::random().into(), ..Default::default()}
-        );
-        cube.set_position(Vector3::new(
-            Math::random() - 0.5,
-            Math::random() - 0.5,
-            Math::random() - 0.5,
-        ));
 
-        let mut mat = StandardMaterial::new(format!("mat_{}", i).as_str(),&scene.borrow());
-        mat.set_diffuse_color(Color3::new(
-            Math::random(),
-            Math::random(),
-            Math::random(),
-        ));
-        mat.set_alpha(Math::random());
-        cube.set_material(&mat);
-        
-        SHAPES.with(|shapes| {
-            shapes.borrow_mut().push(cube);
-        });
-    }
+    GAME.with(|game| {
+        let scene = api::create_basic_scene("#renderCanvas");
+        for i in 0..10 {
+            let mut cube = BabylonMesh::create_box(
+                &scene.borrow(),
+                format!("box_{}", i).as_str(),
+                BoxOptions { depth: Math::random().into(), height: Math::random().into(), width: Math::random().into(), ..Default::default()}
+            );
+            cube.set_position(Vector3::new(
+                Math::random() - 0.5,
+                Math::random() - 0.5,
+                Math::random() - 0.5,
+            ));
+
+            let mut mat = StandardMaterial::new(format!("mat_{}", i).as_str(),&scene.borrow());
+            mat.set_diffuse_color(Color3::new(
+                Math::random(),
+                Math::random(),
+                Math::random(),
+            ));
+            mat.set_alpha(Math::random());
+            cube.set_material(&mat);
+            
+            game.borrow_mut().shapes.push(cube);
+        }
+    });
 }
